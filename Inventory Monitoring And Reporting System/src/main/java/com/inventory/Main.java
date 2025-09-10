398f59cb8db15355f4bac16b6ebc7fda48578684
Sujan Poojary <poojarysujan77@gmail.com>
2025-09-10 22:44:18 +0530

    added crud operations

--- Inventory Monitoring And Reporting System/src/main/java/com/inventory/Main.java
+++ Inventory Monitoring And Reporting System/src/main/java/com/inventory/Main.java
@@ -6,42 +6,31 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        InventoryManager manager = new InventoryManager();
         Scanner sc = new Scanner(System.in);
-        List<product> products = new ArrayList<product>();
 
-        System.out.print("Enter number of products: ");
-        int n = Integer.parseInt(sc.nextLine());
+        int choice;
+        do {
+            System.out.println("\n==== INVENTORY MENU ====");
+            System.out.println("1. Add Product");
+            System.out.println("2. Remove Product");
+            System.out.println("3. Update Product");
+            System.out.println("4. Search Product by Name");
+            System.out.println("5. Display All Products");
+            System.out.println("6. Exit");
+            System.out.print("Enter choice: ");
+            choice = Integer.parseInt(sc.nextLine());
 
-        for (int i = 0; i < n; i++) {
-            System.out.println("\n--- Enter details for Product " + (i + 1) + " ---");
-
-            System.out.print("ID: ");
-            String id = sc.nextLine();
-
-            System.out.print("Name: ");
-            String name = sc.nextLine();
-
-            System.out.print("Quantity: ");
-            int quantity = Integer.parseInt(sc.nextLine());
-
-            System.out.print("Price: ");
-            double price = Double.parseDouble(sc.nextLine());
-            product p=new product(id,name,quantity,price);
-            products.add(p);
-        }
-
-
-        System.out.println("\n---- Product List----");
-        double totalValue = 0.0;
-        for (product p : products) {
-            p.display();
-            totalValue += p.stockValue();
-        }
-
-
-        System.out.printf("TOTAL STOCK VALUE: %.2f", totalValue);
-
-
+            switch (choice) {
+                case 1: manager.addProduct(); break;
+                case 2: manager.removeProduct(); break;
+                case 3: manager.updateProduct(); break;
+                case 4: manager.searchProduct(); break;
+                case 5: manager.displayAll(); break;
+                case 6: System.out.println("Exiting..."); break;
+                default: System.out.println("Invalid choice");
+            }
+        } while (choice != 6);
+    }
 
     }
-}
--- Inventory Monitoring And Reporting System/src/main/java/com/inventory/model/product.java
+++ Inventory Monitoring And Reporting System/src/main/java/com/inventory/model/product.java
@@ -1,28 +1,73 @@
 package com.inventory.model;
 
 public class product {
-    private String id;
+    private int id;
     private String name;
     private int quantity;
     private double price;
+    private String category;
 
 
-    public product(String id, String name, int quantity, double price) {
+    public product(int id, String name, int quantity, double price ,String category) {
         this.id = id;
         this.name = name;
         this.quantity = quantity;
         this.price = price;
+        this.category = category;
     }
     public double stockValue() {
         return quantity * price;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
     public void display() {
         System.out.println("Product ID: " + id);
         System.out.println("Name: " + name);
+        System.out.println("Category: " + category);
         System.out.println("Quantity: " + quantity);
         System.out.println("Price: " + price);
         System.out.println("Stock Value: " + stockValue());
         System.out.println();
         }
+
 
 }
